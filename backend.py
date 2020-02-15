7331ea1e5e66276a601e5b533e2ecfe6f1d3b85c
abdullahhadi <abdullah.hadi360@gmail.com>
2020-02-14 22:54:33 -0500

    update
    token authentification

--- backend.py
+++ backend.py
@@ -53,8 +53,8 @@ def authenticate():
             hashed_password = get_hashed_password(password)
             # hash password -> insert into mongodb - Done
             # create token -> create a random authentification token that doesn't exist already 
-            while
-            secrets.token_urlsafe() 
+            while(token.find_one({'token': token})):
+                secrets.token_urlsafe() 
             # create new user
         else:
             check_password(password, user['password'])
