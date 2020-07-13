9fff36684ab9f8b22603a37ba5003fb82a68cdb3
Elijah Lopez <elijahllopezz@gmail.com>
2020-07-13 18:44:27 -0400

    Update MyService.kt

--- Cloud Copy Mobile/android/app/src/main/kotlin/com/example/clipboard_1/MyService.kt
+++ Cloud Copy Mobile/android/app/src/main/kotlin/com/example/clipboard_1/MyService.kt
@@ -28,16 +28,16 @@ import org.json.JSONObject
 
 
 internal class MyService : IntentService("ClipboardMonitor") {
-    private val baseURL = "http://167.99.191.206/"
-    private val getNewCopyURL = "${baseURL}newest-copy/?token=${token}"
-    private val sendNewCopyURL = "${baseURL}share-copy/"
+    private val API = "http://167.99.191.206/"
+    private val geteNewCopy = "${API}newest-copy/?token=${token}"
+    private val sendNewCopy = "${API}share-copy/"
     private lateinit var KEY: ByteArray
 
     override fun onHandleIntent(intent: Intent?) {
         // what is startForeground?
-        startMonitoring()
+        clipboardUpdate()
 //        thread(start=true, isDaemon=true) {
-//            startMonitoring()
+//            clipboardUpdate()
 //        }
     }
 
@@ -55,7 +55,7 @@ internal class MyService : IntentService("ClipboardMonitor") {
         return mac.doFinal(text.toByteArray())
     }
 
-    fun startMonitoring() {
+    fun clipboardUpdate() {
         val mPrefs = getSharedPreferences("FlutterSharedPreferences", Context.MODE_PRIVATE)
         val email = mPrefs.getString("flutter.email", "")
         val token = mPrefs.getString("flutter.token", "")
@@ -67,67 +67,60 @@ internal class MyService : IntentService("ClipboardMonitor") {
         println("lastUpdate = " + lastUpdate) // TODO: better format
         var currentCopy = ""
         var newCopy = ""
-        // ClipData.newPlainText("text", et_copy_text.text);
-        // myClipboard?.setPrimaryClip(myClip);
-        while (mPrefs.getString("flutter.email", "") == email) {
-            // TODO: try catch no internet error
-            val temp = clipboard?.getPrimaryClip()?.getItemAt(0)?.text
-            if (temp != null) newCopy = temp.toString()
-            // println(newCopy)
-            // https://abhiandroid.com/programming/volley
-            if (newCopy != currentCopy) {
-                println("send copy: ${newCopy}")
-                sendCopy(newCopy)
-                currentCopy = newCopy
-                println("set new copy")
-                val gson = Gson()
-                val httpClient = HttpClientBuilder.create().build()
-                val post = HttpPost(sendNewCopyURL)
-                val map = HashMap<String, String>()
-                map.put("token", token)
-                map.put("content", encrypt(newCopy))
-                post.setEntity(JSONobject(map).toString())
-                post.setHeader("Content-type", "application/json")
-                val response = httpClient.execute(post)
-                println(response.text)
-                // currentCopy = newCopy
-                // val stringRequest = StringRequest(Request.Method.POST, sendNewCopyURL,
-                // post(sendNewCopyURL, data=mapOf("token" to token, "contents" to encrypt(newCopy)))
-            } else {
-                val stringRequest = StringRequest(Request.Method.GET, getNewCopyURL,
-                        Response.Listener { response ->
-                            if (response != "false") {
-                                val dataJSON = gson.fromJson(response, JSONObject::class.java)
-                                var newCopy = dataJSON.get("contents")
-                                var timestamp = dataJSON.get("timestamp")
-                                println(newCopy)
-                                println(timestamp)
-//                                var eg = decrypt(newCopy).toString(Charsets.UTF_8)
-//                                println(eg)
-                                // TODO: string to Date
-                                // myClipboard?.setPrimaryClip(newCopy);
-                            }
-                        }, Response.ErrorListener {
-                    fun onErrorResponse(error: VolleyError?) {
-                        // TODO: handle no internet here
-                    }
-                })
-                // var resp = URL(getNewCopyURL).readText()
-                // if resp != 'false':
-                //     resp = gson.fromJson(resp, Article::class.java)  // now a map
-                //     new_copy = decrypt(resp.get("current_copy")).toString()
-                //     timestamp = resp.get("timestamp")
-                //     if new_copy != current_text and last_update < timestamp:
-                //         last_update = timestamp
-                //         current_text = new_copy
-                //         pyperclip.copy(new_copy)
-                // get request to newest-copy/?token=
-                // check if resp is not false
-                // then check if currentCopy != newCopy and
-                // if the time given > lastUpdate
-                // if false, post a notification to tell user to log in again
+
+        // TODO: try catch no internet error
+
+        fun update() {
+            if (mPrefs.getString("flutter.email", "") == email) {
+                val temp = clipboard?.getPrimaryClip()?.getItemAt(0)?.text
+                if (temp != null) newCopy = temp.toString()
+                // println(newCopy)
+                // https://abhiandroid.com/programming/volley
+                if (newCopy != currentCopy) {
+                    println("send copy: ${newCopy}")
+                    sendCopy(newCopy)
+                    currentCopy = newCopy
+                    println("set new copy")
+                    val gson = Gson()
+                    val httpClient = HttpClientBuilder.create().build()
+                    val post = HttpPost(sendNewCopy)
+                    val map = HashMap<String, String>()
+                    map.put("token", token)
+                    map.put("content", encrypt(newCopy))
+                    post.setEntity(JSONobject(map).toString())
+                    post.setHeader("Content-type", "application/json")
+                    val response = httpClient.execute(post)
+                    println(response.text)
+                    // currentCopy = newCopy
+                    // val stringRequest = StringRequest(Request.Method.POST, sendNewCopy,
+                    // post(sendNewCopy, data=mapOf("token" to token, "contents" to encrypt(newCopy)))
+                } else {
+                    val stringRequest = StringRequest(Request.Method.GET, geteNewCopy,
+                            Response.Listener { response ->
+                                if (response != "false") {
+                                    val dataJSON = gson.fromJson(response, JSONObject::class.java)
+                                    var newCopy = dataJSON.get("contents")
+                                    var timestamp = dataJSON.get("timestamp")
+                                    println(newCopy)
+                                    println(timestamp)
+    //                                var eg = decrypt(newCopy).toString(Charsets.UTF_8)
+    //                                println(eg)
+                                    // TODO: string to Date
+                                    // myClipboard?.setPrimaryClip(newCopy);
+                                }
+                            }, Response.ErrorListener {
+                                fun onErrorResponse(error: VolleyError?) {
+                                    // TODO: handle no internet here
+                                }
+                            })
+                    // get request to newest-copy/?token=
+                    // check if resp is not false
+                    // then check if currentCopy != newCopy and
+                    // if the time given > lastUpdate
+                    // if false, post a notification to tell user to log in again
+                }
+                Handler().postDelayed(update, 1000)
             }
-            Thread.sleep(1500)
         }
     }
 
